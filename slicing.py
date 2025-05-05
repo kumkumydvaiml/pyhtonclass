7a30e1eb3d35d0481ca676a102f52f235d9c50a2
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-06 01:22:12 +0530

    slicing of string

--- slicing.py
+++ slicing.py
@@ -1,8 +1,15 @@
-s="Hello"
-print(s[::-1])
-print(s[::])
-print(s[0:6:2])
-s=s[::][0:1]
-print(s)
+# s="Hello"
+# print(s[::-1])
+# print(s[::])
+# print(s[0:6:2])
+# s=s[::][0:1]
+# print(s)
+
+string="kumkum"
+# print(string[0:3])
+# print(string[-1:-4:2]) doubt
+# print(string[1::2])
+print(string[-1:2:])
+
 
 
