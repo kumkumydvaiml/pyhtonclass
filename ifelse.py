6de829e89239a7b1e33f9d969d7e0dad1b5635d7
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-06 12:48:52 +0530

    if else questions

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+#multiple of 5
+# n= int(input("Enter number :"))
+# if(n%5==0):
+#     print("Multiple of 5")
+# else:
+#     print("Not multiple of 5")
+
+
+
+#Eligible of vote
+# age=int(input("Enter your age :"))
+# if(age>=18):
+#     print("Eligible for vote")
+# else:
+#     print("Not eligible for vote")
+
+
+# password is correct or not
+# password=int(input("Enter password :"))
+# if(password==123):
+#     print("Welcome")
+# else:
+#     print("getout")
+
