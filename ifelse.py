e323c229be484382c6c5c5e9eec03c637419b7ba
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-06 12:38:28 +0530

    vowel or consonant

--- ifelse.py
+++
@@ -1,7 +0,0 @@
-a=10
-b=30
-
-if(a<b):
-        print(a)
-else:
-        print(b)
---
+++ vowelwithlistifelse.py
@@ -0,0 +1,6 @@
+li=['a','e','i','o','u']
+ch=(input("Enter alpahbet :"))
+if(ch in li):
+        print("Vowel")
+else:
+        print("Consonant")
