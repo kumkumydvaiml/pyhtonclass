70b6a624428dfe169fc8ff751309973672fcc736
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-08 23:30:45 +0530

    if and elif

--- ifelse1.py
+++ ifelse1.py
@@ -70,6 +70,21 @@
 # else:
 #     print("Invalid")
 
+dic={
+    1:"coffee",
+    2:"tea",
+    3:"juice"
+}
+a=int(input("Enter option you want in number :"))
+if(a==1):
+    print("Coffee price is 99")
+elif(a==2):
+      print("tea price is 45 rs")
+elif(a==3):
+      print("juice price is 110 rs")
+else:
+    print("try again :(")
+
 
 # marks=int(input("Enter your marks :"))
 # if(marks>0 and marks<30):
@@ -83,33 +98,33 @@
 
 
 
-sal=int(input("Enter salary :"))
-if(sal>0 and sal<=1000):
-    print("You can purchase Nokia")
-    print("Price is 5000rs and 10% gst")
-    print("gst is",sal*10/100)
-    print("After gst mobile price is",sal+sal*10/100)
-    print("Your installment make is 24 months")
-    print("Per month you should pay",(sal+sal*10/100)/24)
-elif(sal>1000 and sal<=5000):
-    print("You can purchase Realme")
-    print("Price is 30000rs and 15% gst")
-    print("gst",sal*15/100)
-    print("After gst mobile price is",sal+sal*15/100)
-    print("Your installment make is 36 months")
-    print("Per month you should pay",(sal+sal*10/100)/36)
+# sal=int(input("Enter salary :"))
+# if(sal>0 and sal<=1000):
+#     print("You can purchase Nokia")
+#     print("Price is 5000rs and 10% gst")
+#     print("gst is",sal*10/100)
+#     print("After gst mobile price is",sal+sal*10/100)
+#     print("Your installment make is 24 months")
+#     print("Per month you should pay",(sal+sal*10/100)/24)
+# elif(sal>1000 and sal<=5000):
+#     print("You can purchase Realme")
+#     print("Price is 30000rs and 15% gst")
+#     print("gst",sal*15/100)
+#     print("After gst mobile price is",sal+sal*15/100)
+#     print("Your installment make is 36 months")
+#     print("Per month you should pay",(sal+sal*10/100)/36)
 
-elif(sal>50000):
-    print("You can purchase iphone")
-    print("Price is 80000rs and 20% gst")
-    print("After gst",sal*20/100)
-    print("After gst mobile price is",sal+sal*20/100)
-    print("Your installment make is 48 months")
-    print("Per month you should pay",(sal+sal*10/100)/48) 
+# elif(sal>50000):
+#     print("You can purchase iphone")
+#     print("Price is 80000rs and 20% gst")
+#     print("After gst",sal*20/100)
+#     print("After gst mobile price is",sal+sal*20/100)
+#     print("Your installment make is 48 months")
+#     print("Per month you should pay",(sal+sal*10/100)/48) 
     
 
-else:
-    print("getout")
+# else:
+#     print("getout")
     
 
     
