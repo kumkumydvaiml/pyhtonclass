288320a9d951b6600636bb4ce6c578cf056a642a
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-12 22:24:50 +0530

    unit with if and else

---
+++ bill.py
@@ -0,0 +1,40 @@
+unit=int(input("Enter unit :"))
+if(unit>0 and unit<=50):
+    print("per unit is 5 rs",unit*5)
+elif(unit>50 and unit<=100):
+    print("per unit is 10 rs",unit*10-1000-250)
+elif(unit>100 and unit<=200):
+    print("per unit is 20 rs",unit*20-4000-1000-250)
+elif(unit>200 and unit<=300):
+    print("per unit is 30 rs",unit*30-9000-4000-1000-250)
+else:
+    print("Invalid")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n1=int(input("Enter number1 :"))
+# n2=int(input("Enter number2 :"))
+# o=input("Enter operator :")
+# if(o=='+'):
+#     print(n1+n2)
+# elif(o=='-'):
+#     print(n1-n2)
+# elif(o=='/'):
+#     print(n1/n2)
+# elif(o=='*'):
+#     print(n1*n2)
+# elif(o=='%'):
+#     print(n1%n2)
+# else:
+#     print("Invalid")
