5e611557f9c54fc87a0c671392d1a0d837f9c6f0
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-21 23:27:46 +0530

    Assiment

---
+++ assignment.py
@@ -0,0 +1,192 @@
+# Que-1 WAP to check whether a number is negative,positive or zero? 
+# n=int(input("Enter number :"))
+# if n>0:
+#     print("Positive")
+# elif n<0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+# Que-2 WAP to check whether a number is divisible by 5 and 11 or not?
+# n=int(input("Enter number :"))
+# if n%5==0 and n%11==0:
+#     print("Divisible of 5 and 11")
+# else:
+#     print("Not divisible of 5 and 11")
+
+# Que-3 WAP to check whether a year is leap year or not?
+# year=int(input("Enter year :"))
+# if year%4==0:
+#     print("Leap year.")
+# else:
+#     print("Not a leap year.")
+
+# Que-4  WAP to find maximum between three numbers ?
+# n1=int(input("Enter number1 :"))
+# n2=int(input("Enter number2 :"))
+# n3=int(input("Enter number3 :"))
+ 
+# if n1>n2 and n1>n3:
+#     print("Greatest number is",n1)
+# elif n2>n1 and n2>n3:
+#     print("Greatest number is",n2)
+# else:
+#     print("Greatest number is",n3)
+
+# Que-5  WAP to find maximum between two numbers ?
+# n1=int(input("Enter number1 :"))
+# n2=int(input("Enter number2 :"))
+
+# if n1>n2:
+#     print("Greatest number is",n1)
+# else:
+#     print("Greatest number is",n2)
+
+# Que-6  WAP to check even or odd ?
+
+# n=int(input("Enter number :"))
+# if n%2==0:
+#     print("No is even.")
+# else:
+#     print("No is odd")
+
+# Que-7  WAP to input all sides of a triangle and check wheather triangle is valid or not ?
+
+# side1=int(input("Enter side1 of triangle :"))
+# side2=int(input("Enter side2 of triangle :"))
+# side3=int(input("Enter side3 of triangle :"))
+
+# sum=side1+side2+side3
+
+# if sum==180:
+#     print("It's a triangle")
+# else:
+#     print("Not a triangle")
+
+# Que-8  WAP to input marks  ?
+
+# m1=int(input("Enter your physics marks :"))
+# m2=int(input("Enter your mathematics marks :"))
+# m3=int(input("Enter your chemistry marks :"))
+# m4=int(input("Enter your biology marks :"))
+# m5=int(input("Enter your computer marks :"))
+
+# marks=m1+m2+m3+m4+m5
+# print(marks)
+# percentage=marks/500*100
+
+# print("Your percentage is ",percentage)
+
+# if percentage>=90:
+#     print("You got Grade A.")
+# elif percentage>=80:
+#     print("You got Grade B ")
+# elif percentage>=70:
+#     print("You got Grade C ")
+# elif percentage>=60:
+#     print("You got Grade D ")
+# elif percentage>=40:
+#     print("You got Grade E ")
+# elif percentage<40:
+#     print("You got Grade F ")
+
+
+
+# Que-9  WAP to input any alphabet and check whether it is vowel or consonant?
+# li=['a','e','i','o','u']
+# ch=input("Enter any alphabet to check vowel or consonant :")
+
+# if ch in li:
+#     print("It's a vowel.")
+# else:
+#     print("It's a consonant.")
+
+
+
+# Que-11  WAP to check whether a character is alphabet or not?
+# ch=input("Enter character :")
+# if (ch>='A' and ch<='Z') or (ch>='a' and ch<='z'):
+#     print("It is a character.")
+# else:
+#     print("Not a character.")
+
+
+
+# Que-12 WAP to check whether a character is lowercase or uppercase alphabet?
+
+# ch=input("Enter any character :")
+# if ch>='a' and ch<='z':
+#     print("Character is lowercase .")
+# elif ch>='A' and ch<='Z':
+#     print("Character is uppercase.")
+# else:
+#     print("Invalid")
+
+
+
+# Que-13 WAP to input week number and print week day ?
+
+# dic={
+#      1:'Monday',
+#      2:'Tuesday',
+#      3:'Wednesday',
+#      4:'Thursday',
+#      5:'Friday',
+#      6:'Saturday',
+#      7:'Sunday'
+#      }
+# day=int(input("Enter days in number :"))
+# if day==1:
+#     print("Monday")
+# elif day==2:
+#     print("Tuesday")
+# elif day==3:
+#     print("Wednesday")
+# elif day==4:
+#     print("Thursday")
+# elif day==5:
+#     print("Friday")
+# elif day==6:
+#     print("Saturday")
+# elif day==7:
+#     print("Sunday")
+# else:
+#     print("Invalid")
+
+# side1=int(input("Enter one side of triangle :"))
+# side2=int(input("Enter second side of triangle :"))
+# side3=int(input("Enter third side of triangle :"))
+
+# if side1==side2 or side1==side3:
+#     print("Isoscale triangle.")
+# elif side1==side2 or side1==side3:
+#     print("Equilateral triangle.")
+# elif side1!=side2 or side1!=side3:
+#     print("Scalene triangle") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
---
+++ pattern.py
@@ -0,0 +1,33 @@
+# for r in range(1,5):
+#  for c in range(1,5):
+#    if c>=r:
+#     print("*",end=" ")
+#    else:
+#     print(" ",end=" ")
+#  print()
+
+
+# rows=5
+# for r in range(1,rows+1):
+#  for c in range(1,rows+1):
+#    if c>=6-r:
+    
+#     print("*",end=" ")
+#    else:
+#     print(" ",end=" ")
+#  print()
+
+
+
+
+# rows=5
+# for r in range(1,rows+1):
+#  for c in range(1,rows+1):
+#    if c>=(rows+1)-r:
+    
+#     print("*",end=" ")
+#    else:
+#     print(" ",end=" ")
+#  print()
+
+
