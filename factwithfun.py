cb4bd0cf2d9e716ef2dbff9dc96c9621fa2261ac
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-05-26 23:44:38 +0530

    functions

--- factwithfun.py
+++ factwithfun.py
@@ -1,34 +1,78 @@
-# def factorial():
-#     a=int(input("Enter a number :"))
-#     ans=1
-#     for i in range(1,a+1):
-#         ans*=i
-#     print(ans)
-# factorial()
+def sum(a,b):
+    return a+b
+
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Number: "))
+
+ans = sum(num1,num2)
+print(ans)
+
+# def perfect_number():
+#     num=int(input("Enter a Number: "))
+#     ans=0
+#     for i in range(1,int(num**0.5)+1):
+#         if num%i==0:
+#             ans+=i
+#     if ans==num:
+#         print("Perfect Number")
+#     else:
+#         print("Not a Perfect Number")
+
+# while(1):
+#     a=int(input("Press 1 to exit press 2 to continue: "))
+#     if a==1:
+#         break
+#     elif a==2:
+#         perfect_number()
+        
+    
 
 
-# def fabonacci():
-#     a=int(input("Enter a number :"))
-#     first=0
-#     second=1
-#     for i in range(a):
-#         if i==0:
-#             print(first)
-#             continue
-#         if i==1:
-#             print(second)
-#             continue
-#         next=first+second
-#         first=second
-#         second=next
-#         print(next)
-# fabonacci()
 
-# def natural():
-#      a=int(input("Enter a number :"))
-#      for i in range(1,a):
-#           print(i)
-# natural()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # def factorial():
+# #     a=int(input("Enter a number :"))
+# #     ans=1
+# #     for i in range(1,a+1):
+# #         ans*=i
+# #     print(ans)
+# # factorial()
+
+
+# # def fabonacci():
+# #     a=int(input("Enter a number :"))
+# #     first=0
+# #     second=1
+# #     for i in range(a):
+# #         if i==0:
+# #             print(first)
+# #             continue
+# #         if i==1:
+# #             print(second)
+# #             continue
+# #         next=first+second
+# #         first=second
+# #         second=next
+# #         print(next)
+# # fabonacci()
+
+# # def natural():
+# #      a=int(input("Enter a number :"))
+# #      for i in range(1,a):
+# #           print(i)
+# # natural()
     
 
     
