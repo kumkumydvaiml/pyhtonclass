893e130f8b2be23ea6ccda3d81a05529201cf910
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-06-17 23:23:33 +0530

    sum

---
+++ hello.py
@@ -0,0 +1,14 @@
+# This program prints a message and adds two numbers
+
+# Print a greeting message
+print("Hello, World!")
+
+# Ask user to enter two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum is:", sum)
