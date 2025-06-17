881bf74ba28c1a3804a2238968aa196759143b38
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-06-17 23:25:30 +0530

    code

--- hello.py
+++ hello.py
@@ -1,14 +1,25 @@
-# This program prints a message and adds two numbers
+# # This program prints a message and adds two numbers
 
-# Print a greeting message
-print("Hello, World!")
+# # Print a greeting message
+# print("Hello, World!")
 
-# Ask user to enter two numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# # Ask user to enter two numbers
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
 
-# Add the numbers
-sum = num1 + num2
+# # Add the numbers
+# sum = num1 + num2
 
-# Print the result
-print("The sum is:", sum)
+# # Print the result
+# print("The sum is:", sum)
+
+
+
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
