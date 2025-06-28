61d682697037b63ddc9d640e1c418dd4a40ffd71
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-06-28 23:40:30 +0530

    print

--- forloopprac.py
+++ forloopprac.py
@@ -158,6 +158,17 @@ if count==2:
             print("prime")
 else:
             print("Not prime") 
+
+
+n=int(input("Enter a number :"))
+count=0
+for i in range(1,n+1):
+    if n%i==0:
+        count+=1
+if count==2:
+            print("prime")
+else:
+            print("Not prime") 
     
 
 
