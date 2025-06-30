0e39d004421f9abf72c29701ec023d105321fc82
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-01 00:00:33 +0530

    code

---
+++ ifelse2.py
@@ -0,0 +1,71 @@
+# n=int(input("Enter a number :"))
+# if n>0:
+#     print("Positive.")
+# elif n<0:
+#     print("Negative.")
+# else:
+#     print("Zero.")
+
+
+# marks=int(input("Enter a number :"))
+# if marks>80 and marks<90:
+#     print("A grade")
+# elif marks>60 and marks<80:
+#     print("B grade")
+# elif marks>40 and marks<60:
+#     print("C grade")
+# else:
+#     print("D grade")
+
+# n=int(input("Enter a number :"))
+# if n%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# n1=int(input("Enter number 1 :"))
+# n2=int(input("Enter a number 2 :"))
+# n3=int(input("Enter a number 3 :"))
+# if n1>n2 and n1>n3:
+#     print("number 1 is largest")
+# elif n2>n1 and n2>n3:
+#     print("number 2 is largest")
+# else:
+#     print("number 3 is largest")
+
+
+# year=int(input("Enter the year :"))
+# if (year%4==0) or (year%400==0 and year%100!=0):
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+
+
+# age=int(input("Enter your age :"))
+# if age>=18:
+#     print("You can vote")
+# else:
+#     print("Not eligible for vote")
+
+
+# ch=['a','e','i','o','u','A','E','I','O','U']
+# ch1=input("Enter any character :")
+# if ch1 in ch:
+#     print("Its a vowel ")
+# else:
+#     print("Consonant")
+
+# pwd=int(input("Enter your password :"))
+# if pwd==1234:
+#     print("Welcome")
+# else:
+#     print("Try again")
+
+
+
+
+
+
+
+
