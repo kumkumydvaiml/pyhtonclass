faec699ba096ba02c98b97fe32819350412fc6ff
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-02 12:35:08 +0530

    code

--- forloop2.py
+++ forloop2.py
@@ -15,15 +15,63 @@
 # sum=0
 # n=1
 # while n<=5:
-#  sum=sum+n
-#  print(sum)
-#  n+=1
-
+#     sum=sum+n
+#     n+=1
+# print(sum)
 
 # n=50
 # for i in range(1,n+1):
 #     if i%3==0 and i%5==0:
 #         print(i,"Divisible by 3 or 5")
+
+# sum=0
+# n=int(input("Enter a number :"))
+# for i in range(1,n):
+#     sum+=i*i
+# print(sum)
+
+# i=1
+# n=int(input("Enter a number :"))
+# while i<=10:
+#     print(n*i)
+#     i+=1
+
+# n=int(input("Enter a number :"))
+# for i in range(2,n):
+#     if n%i==0:
+#         print("Not a prime")
+#         break
+# else:
+#     print("Prime")
+
+# rev=0
+# n=int(input("Enter a number :"))
+# while n>0:
+#     digit=n%10
+#     rev=rev*10+digit
+#     n=n//10
+# print(rev)
+
+first=0
+second=1
+next
+n=int(input("Enter a number :"))
+print(first)
+print(second)
+for i in range(1,n-2):
+    next=first+second
+    first=second
+    second=next
+    print(next)
+
+    
+
+
+
+
+
+
+
 
 
 
