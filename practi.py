239dcff28469a8070876e1ff205a5bdcbbc36709
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-03 23:31:01 +0530

    prime

--- practi.py
+++ practi.py
@@ -101,5 +101,13 @@
 #     print("prime")
 
 
-
-
+n=int(input("Enter a number :"))
+is_prime=True
+for i in range(2,int(n**0.5)+1):
+    if n%i==0:
+        is_prime=False
+        break
+if is_prime and n>1:
+    print(n,"is a prime member")
+else:
+    print(n,"is not a prime member")
--- prime.py
+++ prime.py
@@ -1,3 +1,4 @@
+#1st method of prime number ?
 n=int(input("Enter number :"))
 count=0
 
@@ -9,3 +10,18 @@ if count==2:
     print("Prime")
 else:
     print("Not a prime")
+
+
+
+# 2nd method of prime number ?
+
+n=int(input("Enter a number :"))
+is_prime=True
+for i in range(2,int(n**0.5)+1):
+    if n%i==0:
+        is_prime=False
+        break
+if is_prime and n>1:
+    print(n,"is a prime member")
+else:
+    print(n,"is not a prime member")
