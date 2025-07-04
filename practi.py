b18c9c37b26879e01b6404da2ad5a164414455d7
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-05 01:00:18 +0530

    codes

--- practi.py
+++ practi.py
@@ -163,6 +163,13 @@
 #     print("Not an armstrong")
 
 
+# word='Education'
+# count=0
+# for char in word:
+#     if char in word:
+#         count+=1
+# print(f"{count} Letter in {word} ")
+
 
 
 
