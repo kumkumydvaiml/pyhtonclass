979ea06f4d5be1670486def78c5dbecf8fd90a1e
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-06 01:26:53 +0530

    print star

--- forloop2.py
+++ forloop2.py
@@ -127,10 +127,10 @@
 #     print()
 
 
-for i in range(6,-1):
-    for j in range(6,-1):
-        print("*",end=" ")
-    print()
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
 
 
 
