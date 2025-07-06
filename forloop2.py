c55f56d330d87bc6bb4fb0e5a512d617fdb00746
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-06 18:36:19 +0530

    code

--- forloop2.py
+++ forloop2.py
@@ -137,10 +137,12 @@
 #         print(j,end=" ")
 #     print()
 
-for i in range(5,0,-1):
-    for j in range(i,0,-1):
-        print(j,end=" ")
-    print()
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
 
 
 
