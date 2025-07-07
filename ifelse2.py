5d583b779727c1f4dad6e22b7d6af21c0e610e16
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-08 00:51:27 +0530

    d

--- ifelse2.py
+++ ifelse2.py
@@ -79,32 +79,34 @@
 
 
 
-n=int(input("Enter 1 :"))
-if n==1:
-    n=int(input("Enter 2,3 or 4 any one :"))
-    if n==2:
-        n=int(input("Enter 3 or 4 :"))
-    if n==3:
-        n=int(input("Enter 4 :"))
-        if n==4:
-            print("Reached")
-    elif n==4:
-        print("reached")
-    else:
-        print("Invalid")
+# n=int(input("Enter 1 :"))
+# if n==1:
+#     n=int(input("Enter 2,3 or 4 any one :"))
+#     if n==2:
+#         n=int(input("Enter 3 or 4 :"))
+#     if n==3:
+#         n=int(input("Enter 4 :"))
+#         if n==4:
+#             print("Reached")
+#     elif n==4:
+#         print("reached")
+#     else:
+#         print("Invalid")
 
-elif n==3:
-    n=int(input("Enter 2 or 4 :"))
-    if n==2:
-        n=int(input("Enter 4 :"))
-        if n==4:
-            print("Reached")
-        else:
-            print("Invalid")
-elif n==4:
-    print("Reached")
-else:
-    print("Invalid Input")
+# elif n==3:
+#     n=int(input("Enter 2 or 4 :"))
+#     if n==2:
+#         n=int(input("Enter 4 :"))
+#         if n==4:
+#             print("Reached")
+#         else:
+#             print("Invalid")
+# elif n==4:
+#     print("Reached")
+# else:
+#     print("Invalid Input")
+
+
 
 
   
