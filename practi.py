507617b06e9e48f1c76c755db55385ec8cb5897e
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-10 00:50:08 +0530

    0

--- practi.py
+++ practi.py
@@ -350,8 +350,13 @@
 #     print()
 
 
-for i in range(6,0,-1):
-    for j in range(i):
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+for i in range(1,10):
+    for j in range(i+1):
         print("*",end=" ")
     print()
         
