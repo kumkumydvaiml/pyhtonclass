76db5a7253a25e1bbe3807c39174aadfea4959f4
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-12 00:26:21 +0530

    i

--- pp.py
+++ pp.py
@@ -4,19 +4,26 @@
 # p=input("Choose any item :")
 
 
-li=["papaya","grapes","Apple"]
-print(type(li))
-print(li)
-p=input("Choose any item :")
+# li=["papaya","grapes","Apple"]
+# print(type(li))
+# print(li)
+# p=input("Choose any item :")
 
-li=["papaya","grapes","Apple"]
-print(type(li))
-print(li)
-p=input("Choose any item :")
+# li=["papaya","grapes","Apple"]
+# print(type(li))
+# print(li)
+# p=input("Choose any item :")
 
 
-li=["papaya","grapes","Apple"]
-print(type(li))
-print(li)
-p=input("Choose any item :")
+# li=["papaya","grapes","Apple"]
+# print(type(li))
+# print(li)
+# p=input("Choose any item :")
+
+
+i=1
+while i<=20:
+    if i%2==0:
+        print("even numbers are",i)
+    i+=1
 
