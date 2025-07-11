82f521d909db58b570c3a69fe2ecba4e96f631dc
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-11 13:16:41 +0530

    py

--- sumoflist.py
+++ sumoflist.py
@@ -1,7 +1,28 @@
-pro=1
-li=[1,2,3]
+# ans=0
+# li=[2,5,4,6]
+# for i in li:
+#     ans=ans+i
+# print(ans)
+
+# print(sum(li))
+
+
+# pro=1
+# li=[1,2,3]
+# for i in li:
+#     pro*=i
+# print(pro)
+
+
+lar=0
+li=[5,6,10,8,4]
 for i in li:
-    pro*=i
-print(pro)
+    if i>lar:
+        lar=i
+print(lar)
+
+
+
+
 
 
