51b15375c497cd9ed4ac653041d5b21e9fde8c45
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-12 13:18:06 +0530

    pyth

---
+++ li.py
@@ -0,0 +1,23 @@
+#count the element given by user
+# li=[1,2,1,2,3,1]
+# n=int(input("Enter number from the list :"))
+# count=0
+# for i in li:
+#     if n==i:
+#         count+=1
+# print(count)
+
+
+#display the indexing of element given by user ?
+# li1=[1,2,3,5,4]
+# n=int(input("Enter number from the list :"))
+
+# for i in range(len(li1)):
+#     if li1[i]==n:
+#         print(i)
+
+# reverse a given list
+li=[1,2,3,4,5]
+
+        
+
