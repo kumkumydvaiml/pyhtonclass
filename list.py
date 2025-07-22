66b1e9a425252dde874e0a830e3ab6a43175f053
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-22 13:28:42 +0530

    p

---
+++ list.py
@@ -0,0 +1,46 @@
+# topic=operators in list
+# l=[1,2,"python","java"]
+# print(max(l))
+# print(min(l))
+# # print(len(l))
+# # print(type(l))
+# # print(id(l))
+# # print(list())
+# print(sum(l))
+
+# # list=[1,2,3]
+# # print(list)
+# list.append(4)
+# list.append(4)
+
+# l=[10,20,10.5,10+5j]
+# print(sum(l))
+# print(max(l))
+# print(min(l))
+
+
+# l=[10,20,10.5]
+# print(max(l))
+# print(min(l))
+
+# l1=[1,2,3,4,"python"]
+# l2=["java"]
+# print(l1+l2)
+# print(l1-l2) # do list ke bich kaam nahi karta hai error thrpw karta hai
+# print(l1*l2)
+l1=[1,2,3,4]
+l2=[1,2,3,4]
+
+print(l1*3)
+print(4 in l1)
+print(l1==l2)
+print(l1 is l2)
+
+
+
+
+
+
+
+
+
