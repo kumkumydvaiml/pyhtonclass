f07db9bcd99d251e0029fb6bcd70cf472025c420
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-23 13:34:31 +0530

    python code

---
+++ list_method1.py
@@ -0,0 +1,54 @@
+l=[2,4,"python","java","php",1,5,10,2,3]
+# (l.append(50))
+# print(l)
+
+# x=10
+# y=print(x)
+# print(y)//
+
+# l.extent(itrat)
+# l.extend("python")
+# print(l)
+
+# l.extend([1,2,3,4])
+# print(l)
+
+# l.extend((-1,-2,-3))
+# print(l)
+
+
+# x=l.copy()
+# print(x)
+# print(l)
+# print(id(x),id(l))
+
+# l.clear()
+# print(l)
+
+# del l # del is used to released memory address its a keyword
+# print(l)
+
+# print(l.count(3))
+
+# print(l.index("python"))
+
+# l.insert(5,"kumkum")
+# print(l)
+
+# print(l.pop())
+# print(l)
+
+# l.remove("python")
+# print(l)
+
+# l.reverse()
+# print(l)
+
+l=['name','age','qualification']
+# l.sort()
+# l.reverse()
+l.sort(reverse=True)
+print(l)
+
+
+
