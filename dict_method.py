b60a8d86f32df054c293e65c79fb5d139ec7f2a1
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-25 13:36:21 +0530

    code

---
+++ dict_method.py
@@ -0,0 +1,37 @@
+d={'name':'kumkum',
+   'age':'37',
+   'quali':'mtech'}
+print(d.keys())
+print(d.values())
+print(d.items())
+print(d.get('name'))
+
+d1={
+    1:'python',
+    2:'php'
+}
+d.update(d1)
+print(d)
+
+d.setdefault('name1','rahul')
+print(d)
+
+l=['name','age','city','contact']
+d=dict.fromkeys(l)
+print(d)
+
+d['name']='neeraj'
+print(d)
+
+d.pop('age')
+print(d)
+
+d.popitem()
+print(d)
+
+
+
+
+
+
+
