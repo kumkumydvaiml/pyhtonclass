bb65a6250f5e6f0134e17996d6ffea953a8dbb99
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-25 13:00:25 +0530

    code

---
+++ dic_function.py
@@ -0,0 +1,15 @@
+my_dict={
+1:'kumkum',
+2:37,
+3:'M.tech'
+         }
+
+print(max(my_dict))
+print(min(my_dict))
+print(len(my_dict))
+print(id(my_dict))
+print(dict())
+print(type(my_dict))
+print(sum(my_dict))
+
+
