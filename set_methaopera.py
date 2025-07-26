73d7052db38ea8aef239578289b7b825a5d5855d
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-26 13:15:32 +0530

    c

--- set_methaopera.py
+++
@@ -1,5 +0,0 @@
-# Methametical opeartions of set
-
-s1={1,2,3,4,5}
-s2={2,3,6,7,8}
-print(s1.difference(s2))
---
+++ set_opera.py
@@ -0,0 +1,7 @@
+# Methametical opeartions of set
+
+s1={1,2,3,4,5}
+s2={2,3,6,7,8}
+print(s1.difference(s2))
+print(s1.intersection(s2))
+print(s1.symmetric_difference(s2))
