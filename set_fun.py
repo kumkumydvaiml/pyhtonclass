d2a6ddbf4a4ac6b92a67e6ed431c19b2dbf13753
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-26 13:04:54 +0530

    b

--- set_fun.py
+++ set_fun.py
@@ -1,2 +1,12 @@
-my_set={1,2,3,2,'python',9}
+# my_set={1,2,3,2,'python',9}
+my_set={'python','java','C'}
 print(my_set)
+
+#In buit python fuctions in set
+print(len(my_set))
+print(min(my_set))
+print(max(my_set))
+print(type(my_set))
+# print(sum(my_set)) throw error
+print(id(my_set))
+
