6626e307aab253f2d2c8187c52bb57f603c62d46
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-30 14:36:05 +0530

    code

--- inbuilt_fun.py
+++ inbuilt_fun.py
@@ -6,15 +6,25 @@
 # print(count)
 
 # to count the char
-s1='kumkum'
+s1='madam'
 # target='k'
 # count=0
 # for _ in s1:
 #     if _==target:
 #         count+=1    
 # print(count)
+# s=''
+# for i in range(len(s1)-1,-1,-1):
+#     s+=s1[i]
+# print(s)
+
 s=''
 for i in range(len(s1)-1,-1,-1):
-    s+=s1[i]
-print(s)
+    s=s+s1[i]
+if s1==s:
+    print('palindrome')
+else:
+    print('not a palindrome')
+
+
 
