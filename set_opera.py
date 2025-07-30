1067c1d83fe05d02ca05fc3e4fda90653146f21f
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-30 13:25:52 +0530

    py

--- set_opera.py
+++ set_opera.py
@@ -1,46 +1,51 @@
-# Mathametical opeartions of set
+# # Mathametical opeartions of set
 
-s1={1,2,3,4,5}
-s2={2,3,6,7,8}
-print(s1.difference(s2))
-print(s1.intersection(s2))
-print(s1.symmetric_difference(s2))
+# s1={1,2,3,4,5}
+# s2={2,3,6,7,8}
+# print(s1.difference(s2))
+# print(s1.intersection(s2))
+# print(s1.symmetric_difference(s2))
 
-s1.symmetric_difference_update(s2)
-print(s1)
-s=set()
-s.add('python')
-print(s)
-
-s.update('python')
-print(s)
-# s.update(10,20,30,40)
+# s1.symmetric_difference_update(s2)
+# print(s1)
+# s=set()
+# s.add('python')
 # print(s)
 
-s.update('java','php')
-print(s)
+# s.update('python')
+# print(s)
+# # s.update(10,20,30,40)
+# # print(s)
 
-s.update('p','y','t','h','o','n')
-print(s)
-
-print(s.pop())
-print(s)
-
-s.remove('j')
-print(s)
-
-# s.remove('life')
+# s.update('java','php')
 # print(s)
 
-s.discard('life')
-print(s)
+# s.update('p','y','t','h','o','n')
+# print(s)
 
-s1=s.copy()
-print(s1,s)
-print(id(s1),id(s2))
+# print(s.pop())
+# print(s)
 
-s.clear()
-print(s)
+# s.remove('j')
+# print(s)
+
+# # s.remove('life')
+# # print(s)
+
+# s.discard('life')
+# print(s)
+
+# s1=s.copy()
+# print(s1,s)
+# print(id(s1),id(s2))
+
+# s.clear()
+# print(s)
+
+
+x=range(-1,-1,-1)
+print(list(x))
+
 
 
 
