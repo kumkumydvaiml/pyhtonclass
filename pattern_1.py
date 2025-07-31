16b0a2970d2607011e3a7757797d9e36d6b1b134
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-07-31 13:31:45 +0530

    code

--- pattern_1.py
+++ pattern_1.py
@@ -1,8 +1,22 @@
-n=int(input("Enter number :"))
-i=1
-while i<=n:
-    # print("*"*i)
-    # i=i+1
-    print(' '*(n-i)+'*_'*i)
+# n=int(input("Enter number :"))
+# i=1
+# while i<=n:
+#     # print("*"*i)
+#     # i=i+1
+#     print(' '*(n-i)+'*_'*i)
+#     i+=1
+
+
+# n=int(input("Enter number :"))
+# i=5
+# while i>=1:
+#     print('*'*i)
+#     i-=1
+
+n=5
+i=0
+while i<n:
+    print('*'*(n-i))
     i+=1
+
 
