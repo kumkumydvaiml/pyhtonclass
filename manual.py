4fb310c1eab04d2d5d2b58d8a22531abb4f8931a
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-06 13:38:44 +0530

    code

--- manual.py
+++ manual.py
@@ -1,6 +1,35 @@
-s="kumkum yadav"
-result=""
-for ch in s:
-    if s!=" ":
-        result+=ch
-print(result)
+# s="kumkum yadav"
+# result=""
+# for ch in s:
+#     if s!=" ":
+#         result+=ch
+# print(result)
+
+# n=5
+# i=1
+# while i<=n:
+#     print(' '*(n-i)+'* '*i)
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>0:
+#     print(' '*(n-i)+'* '*i)
+#     i=i-1
+
+
+# n=5
+# for i in range(1,n+1):
+#     print('*'*i)
+
+
+# s='python'
+# s1=""
+# for i in s:
+#     print(chr(ord(i)+1))
+#     s1=''.join([s1,chr(ord(i)+1)])
+
+s=input("Enter :")
+s1=''
+print(s1)
+
+
