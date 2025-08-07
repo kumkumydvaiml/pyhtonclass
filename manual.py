01ffd16560d97ed47c3de3611ccfccf3c12b2fdc
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-08 00:46:13 +0530

    code

--- manual.py
+++ manual.py
@@ -1,30 +1,40 @@
-s='kumkum'
-s1=''
-for i in range(len(s)-1,-1,-1):
-    s1+=s[i]
-print(s1)
+# s='kumkum'
+# s1=''
+# for i in range(len(s)-1,-1,-1):
+#     s1+=s[i]
+# print(s1)
 
-n=input("Enter :")
-result=''
-for ch in n:
-    if ch>='A' or ch<='Z':
-        result+=chr(ord(ch)+32)
-print(result)
+# n=input("Enter :")
+# result=''
+# for ch in n:
+#     if ch>='A' or ch<='Z':
+#         result+=chr(ord(ch)+32)
+# print(result)
 
-n=input("Enter :")
-n1=''
-for ch in n:
-    if n>='a' or n<='z':
-        n1+=chr(ord(ch)-32)
-print(n1)
+# n=input("Enter :")
+# n1=''
+# for ch in n:
+#     if n>='a' or n<='z':
+#         n1+=chr(ord(ch)-32)
+# print(n1)
 
 
-s=input("Enter :")
-s1=''
-for ch in s:
-    if ch!=' ':
-        s1+=ch
-print(s1)
+# s=input("Enter :")
+# s1=''
+# for ch in s:
+#     if ch!=' ':
+#         s1+=ch
+# print(s1)
+
+# s=input("Enter :")
+# s1=''
+# for ch in s:
+#     if ch not in 'aeiouAEIOU':
+#         s1+=ch
+# print(s1) 
+
+
+
 
 
 
