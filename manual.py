a805ca32e713376125b47ff0fd1ba11a87b61454
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-08 00:21:05 +0530

    code

--- manual.py
+++ manual.py
@@ -10,3 +10,10 @@ for ch in n:
     if ch>='A' or ch<='Z':
         result+=chr(ord(ch)+32)
 print(result)
+
+n=input("Enter :")
+n1=''
+for ch in n:
+    if n>='a' or n<='z':
+        n1+=chr(ord(ch)-32)
+print(n1)
