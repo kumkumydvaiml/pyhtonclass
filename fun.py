454737260290efa05b484bca3d7b7803dd52c6fc
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-09 00:33:05 +0530

    code

--- fun.py
+++ fun.py
@@ -1,12 +1,12 @@
-a=10
-b=20
-c=30
-print(all((a>b,a>c)))
+# a=10
+# b=20
+# c=30
+# print(all((a>b,a>c)))
 
-print(any((a<b,a>c)))
+# print(any((a<b,a>c)))
 
 
-n=int(input("Enter :"))
+# n=int(input("Enter :"))
 # i=1
 # while i<=n:
 #     print(i)
@@ -14,9 +14,18 @@ n=int(input("Enter :"))
 # else:
 #     print("Thanks")
 
-for i in range(1,n+1):
-    print(i)
-else:
-    print("Thanks")
+# for i in range(1,n+1):
+#     print(i)
+# else:
+#     print("Thanks")
 
     #independent hai if
+
+    #operater use in datatype
+
+s1 = "class"
+s = "python"
+
+for i in s:
+    s1 = ''.join([s1,i])
+print(s1)    
