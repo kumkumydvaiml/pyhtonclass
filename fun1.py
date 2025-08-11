23a2b7f1eb5eca993567662571fa5d0aed3466b8
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-11 13:23:46 +0530

    code

--- fun1.py
+++ fun1.py
@@ -1,9 +1,31 @@
+# def add(x,y):
+#     # print(x+y)
+#     return(x,y)
+# p=int(input("Enter first value :"))
+# q=int(input("Enter second value :"))
+# result=add(p,q)
+# print(result)
+# print(result)
+# print(result)
+
+
 def add(x,y):
-    print(x+y)
+    return(x+y)
 p=int(input("Enter first value :"))
 q=int(input("Enter second value :"))
 result=add(p,q)
-print(result)
-print(result)
-print(result)
+x=add(p,q)
+if x%2==0:
+    print(f' given no{x} is even')
+else:
+    print('odd')
 
+# koi bhi fuction terminate hota hai return se
+
+def add(x,y):
+    # print(x+y)
+    return(x,y)
+    print("HEllo")
+p=int(input("Enter first value :"))
+q=int(input("Enter second value :"))
+result=add(p,q)
