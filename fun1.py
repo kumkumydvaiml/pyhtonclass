bd9e40d59a522c542f0806173fc4342dc8de81e5
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-12 13:01:47 +0530

    code

--- fun1.py
+++ fun1.py
@@ -42,3 +42,20 @@ def fun_name(x,y,z):
 # print(result)
 #jitne parameter define honge utne hi argumnet dene hai otherwiese error
 
+#default argument
+def fun_name(x=0,y=0,z=0):
+    return x+y+z
+result=fun_name()
+print(result)
+result=fun_name(10)
+print(result)
+result=fun_name(10,20)
+print(result)
+result=fun_name(10,20,30)
+print(result)
+
+#variable length positional 
+def add(*x):
+    print(x)
+    print(type(x))
+result=add(10,20,30,1,2,3,4)
