41d2f9e43e15025685429f518c3f7b62ab6450e5
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-13 13:18:22 +0530

    code

--- eval_1.py
+++ eval_1.py
@@ -1,10 +1,17 @@
-x=eval(input("Enter any value :"))
-print(x)
-print(type(x))
+# x=eval(input("Enter any value :"))
+# print(x)
+# print(type(x))
 
 # x=eval('2+3-1*5')
 # print(x)
 
+
+x=((1,2,3,4,6),)
+sum=0
+for i in x:
+    for j in i:
+        sum+=j
+print(sum)
 
 
 
