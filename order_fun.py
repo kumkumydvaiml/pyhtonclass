37ae2540a16730aa3f9f49da36211891a918e5b0
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-18 13:18:46 +0530

    code

--- order_fun.py
+++ order_fun.py
@@ -1,7 +1,27 @@
-l=[1,2,3,4,5]
-def squr(n):
-    return n**2
-result=map(squr,l)
-print(result)
-print(tuple(result))
+# l=[1,2,3,4,5]
+# def squr(n):
+#     return n**2
+# result=map(squr,l)
+# print(result)
+# print(tuple(result))
+# print(list(result))
+
+
+# li1=[6,7,8,9,10]
+# li2=[1,2,3,4,5]
+# li3=[1,2,3,4]
+# li4=[1,2,3]
+# def add(x,y,z,p):
+#     return x+y+z+p
+# result=map(add,li1,li2,li3,li4)
+# print(list(result))
+
+
+li1=[6,7,8,9,10]
+li2=[1,2,3,4,5]
+li3=[1,2,3,4,5]
+
+def add(x,y,z):
+    return x+y+z
+result=map(add,li1,li2,li3)
 print(list(result))
