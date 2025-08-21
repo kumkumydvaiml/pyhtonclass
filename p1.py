7131538c90f2746659f0fe8939f7960f500d6990
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-21 19:18:38 +0530

    code

--- p1.py
+++ p1.py
@@ -15,14 +15,45 @@
 # grade.sort()
 # print(grade)
 
-x,y,z='kuki','mohni','babita'
-print(x)
-print(z)
-print(y)
+# x,y,z='kuki','mohni','babita'
+# print(x)
+# print(z)
+# print(y)
 
-a=b=c='kuki'
-print(a)
-print(b)
-print(c)
+# a=b=c='kuki'
+# print(a)
+# print(b)
+# print(c)
+
+
+
+n=[1,2,3,4,5]
+print(' choose any one \n 1= +\n 2= -\n 3= % \n 4= *\n 5= off\n')
+while True:
+    n0=(1,2,3,4)
+    n=int(input("Enter any one :"))
+    if n==1:
+        n1=int(input("Enter number 1 :"))
+        n2=int(input("Enter number 2 :"))
+        n3=n1+n2
+        print(f'sum of {n1} and {n2} is = {n3}')
+    elif n==2:
+        n1=int(input("Enter number 1 :"))
+        n2=int(input("Enter number 2 :"))
+        n3=n1-n2
+        print(f'subtraction of {n1} and {n2} is = {n3}')
+    elif n==3:
+        n1=int(input("Enter number 1 :"))
+        n2=int(input("Enter number 2 :"))
+        n3=n1%n2
+        print(f'divide of {n1} and {n2} is = {n3}')
+    elif n==4:
+        n1=int(input("Enter number 1 :"))
+        n2=int(input("Enter number 2 :"))
+        n3=n1*n2
+        print(f'multiplication of {n1} and {n2} is = {n3}')
+    else:
+        break
+
 
 
