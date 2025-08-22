fa291dac158df8e811719721329726b46e4b0992
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-22 13:03:32 +0530

    code

---
+++ second.py
@@ -0,0 +1,18 @@
+# from first import x,y,add,calculator
+# print(x)
+# print(y)
+# r=int(input())
+# s=int(input())
+# res=add(r,s)
+# obj=calculator()
+# obj.add(r,s)
+
+
+import first
+print(first.x)
+print(first.y)
+r=int(input('Enter :'))
+s=int(input('Enter :'))
+res=first.add(r,s)
+obj=first.calculator()
+obj.add(r,s)
