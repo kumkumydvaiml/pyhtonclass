235d5240e37f74086c119d7a0c87b57b34b08293
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-27 12:48:53 +0530

    code

--- file_seek.py
+++ file_seek.py
@@ -1,5 +1,11 @@
+#27/aug/2025
 with open('n1.txt','a+') as f:
     print(f.name)
     print(f.mode)
     print(f.closed)
 print(f.closed)
+
+
+with open('n1.txt','a+') as f:
+    print(f.tell())
+    
