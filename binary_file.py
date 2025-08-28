d9a6f71901517ad85fe06be1ac3d35a530130366
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-28 13:27:49 +0530

    code

---
+++ binary_file.py
@@ -0,0 +1,27 @@
+# 28/aug/25
+#topic-- binary file
+
+
+# import pickle
+# f=open('n1.pkl','ab+')
+# data={'name':'kumkum',
+#       'age':20,
+#       'quali':'btech'}
+# pickle.dump(data,f)
+# f.close()
+
+
+# import pickle
+# f=open('n_2.pkl','ab+')
+# data=[1,2,3,4]      # append data
+# pickle.dump(data,f)
+# f.close()
+
+# import pickle
+# f=open('n_2.pkl','rb+')  #read data
+# data=pickle.load(f)
+# print(data)
+# f.close()
+
+
+
