2c6f2b53df07cbf0917681b1e122efbe86d01c65
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-08-30 13:46:37 +0530

    code

--- loc_var.py
+++ loc_var.py
@@ -18,7 +18,38 @@ print(x)
 
 s="kumkum"
 s1=""
-for i in range((s)-1,-1,-1):
+for i in range(len(s)-1,-1,-1):
     s1+=s[i]
 print(s1)
+
+
+s="kumkum"
+s1=""
+for i in range(len(s)-1,-1,-1):
+    s1=''.join([s1,s[i]])
+print(s1)
+
+
+a="kumkum"
+a1=[]
+for i in a:
+    a1.append(ord(i))
+print(a1)
+
+s="pantuation"
+s1=""
+for i in s:
+    if i in s1:
+        s1+=i
+print(s1)
+
+a="kumkum"
+a1=""
+for i in a:
+    if ord(i)%2==0:
+        a1=a1+chr(ord(i))
+print(a1)
+
+
+
 
