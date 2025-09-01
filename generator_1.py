b698eef7fdaece76f3a5c92b727a65faced2c284
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-01 13:26:37 +0530

    code

--- generator_1.py
+++ generator_1.py
@@ -8,9 +8,7 @@ print(res)
 # yield
 def natural_no(n):
     for i in range(1,n+1):
-        if i%2!=0:
-        # if i%2==0:
-            yield i
+        yield i
 x=int(input("Enter number :"))
 res=natural_no(x)
 print(res)
@@ -22,5 +20,6 @@ print("hii")
 print(next(res))
 for i in res:
     print(i)
+    
 
 
