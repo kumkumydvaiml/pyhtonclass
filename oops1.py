cc6e0619de63e57945075f6bcd173562d730056d
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-02 13:32:58 +0530

    code

--- oops1.py
+++ oops1.py
@@ -14,3 +14,20 @@ print(id(obj))
 class New:
     "for demo purpose"
 obj=New
+
+
+class New:
+    "for demo purpose"
+obj=New
+print(dir(New))
+print(New.__doc__)
+
+
+class New:
+    "for demo purpose"
+obj=New
+obj2=New
+obj3=New
+obj4=New
+print(id(New))
+print(id(obj)),id(obj2),id(obj3),id(obj4)
