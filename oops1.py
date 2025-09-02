548a022ae14e1e41867ffbdb72516e40f604096a
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-02 13:36:38 +0530

    code

--- oops1.py
+++ oops1.py
@@ -1,26 +1,26 @@
-class New:
-    x=10
-    y=20
-obj=New
-print(id(New))
-print(id(obj))
+# class New:
+#     x=10
+#     y=20
+# obj=New
+# print(id(New))
+# print(id(obj))
 
-class New:
-    pass
-obj=New
-print(id(New))
-print(id(obj))
+# class New:
+#     pass
+# obj=New
+# print(id(New))
+# print(id(obj))
 
-class New:
-    "for demo purpose"
-obj=New
+# class New:
+#     "for demo purpose"
+# obj=New
 
 
-class New:
-    "for demo purpose"
-obj=New
-print(dir(New))
-print(New.__doc__)
+# class New:
+#     "for demo purpose"
+# obj=New
+# print(dir(New))
+# print(New.__doc__)
 
 
 class New:
@@ -30,4 +30,4 @@ obj2=New
 obj3=New
 obj4=New
 print(id(New))
-print(id(obj)),id(obj2),id(obj3),id(obj4)
+print(id(obj),id(obj2),id(obj3),id(obj4))
