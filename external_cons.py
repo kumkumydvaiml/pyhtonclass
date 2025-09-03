ab804aeecc1a3d9fe398d20b1a3a674575fc627a
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-03 13:36:44 +0530

    code

--- external_cons.py
+++ external_cons.py
@@ -13,3 +13,16 @@ class Student:
         print(id(self))
 x=Student()
 print(id(x),id(Student))
+
+
+
+class Student:
+    def __init__(self,name,grade):
+        self.n=name
+        self.g=grade
+        print("obj self id=",id(self))
+    def account(self):
+        print(self.n)
+        print(self.g)
+obj=Student("Neeraj","10th")
+obj.account()
