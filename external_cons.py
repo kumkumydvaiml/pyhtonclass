f0211b68423ae0fcde098e4dc9f564b243675e35
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-03 13:05:09 +0530

    code

--- external_cons.py
+++ external_cons.py
@@ -5,3 +5,10 @@ x=Student
 print(id(x),id(Student))
 y=Student()
 # print(id(y),id(Student()))
+
+
+class Student:
+    def __init__(self):
+        print("External constructor called")
+x=Student()
+print(id(x),(id(Student)))
