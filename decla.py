269cc3628eeac905611cfbb3caad4450ac613e3d
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-05 13:12:19 +0530

    code

---
+++ decla.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,name,age,roll):
+        self.n=name
+        self.a=age
+        self.r=roll
+    def add_new(self,contact):
+        self.c=contact
+    def show_detail(self):
+        print(self.n,self.a,self.r,self.c,self.e)
+obj=Student('Neeraj',37,10)
+obj.add_new(12345)
+obj.e='Neeraj@gmail.com'#declaration
+obj.show_detail()
