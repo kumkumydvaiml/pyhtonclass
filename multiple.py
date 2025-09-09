c3dd8c720728e926c069625770be43ac705a0c3e
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-09 13:33:13 +0530

    code

--- multiple.py
+++ multiple.py
@@ -1,25 +1,37 @@
-class Parent1:
+# class Parent1:
+#     def home(self):
+#         print("From parent")
+# class Parent2:
+#     def home(self):
+#         print("from parent2")
+# class Child(Parent1,Parent2):
+#     pass
+
+# obj=Child()
+# obj.home()
+
+
+
+# class Parent1:
+#     def home(self):
+#         print("From parent")
+# class Parent2:
+#     def home(self):
+#         print("from parent2")
+# class Child(Parent2,Parent1): # mro
+#     pass
+
+# obj=Child()
+# obj.home()
+
+
+
+class Parent:
     def home(self):
         print("From parent")
-class Parent2:
+class Child(Parent):
     def home(self):
-        print("from parent2")
-class Child(Parent1,Parent2):
-    pass
-
-obj=Child()
-obj.home()
-
-
-
-class Parent1:
-    def home(self):
-        print("From parent")
-class Parent2:
-    def home(self):
-        print("from parent2")
-class Child(Parent2,Parent1):
-    pass
-
+        print("from child")
+        super().home()
 obj=Child()
 obj.home()
