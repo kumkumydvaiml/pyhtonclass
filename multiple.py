6fd34c6e2e6a826ae4510488e493a1aeafa47946
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-09 13:24:47 +0530

    code

--- multiple.py
+++ multiple.py
@@ -9,3 +9,17 @@ class Child(Parent1,Parent2):
 
 obj=Child()
 obj.home()
+
+
+
+class Parent1:
+    def home(self):
+        print("From parent")
+class Parent2:
+    def home(self):
+        print("from parent2")
+class Child(Parent2,Parent1):
+    pass
+
+obj=Child()
+obj.home()
