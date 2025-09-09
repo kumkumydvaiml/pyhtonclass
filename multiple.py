0dcfa141f9832f1193cbdcc3ea636cc8bbeba228
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-09 14:03:59 +0530

    code

--- multiple.py
+++ multiple.py
@@ -1,40 +1,61 @@
-# class Parent1:
+# # class Parent1:
+# #     def home(self):
+# #         print("From parent")
+# # class Parent2:
+# #     def home(self):
+# #         print("from parent2")
+# # class Child(Parent1,Parent2):
+# #     pass
+
+# # obj=Child()
+# # obj.home()
+
+
+
+# # class Parent1:
+# #     def home(self):
+# #         print("From parent")
+# # class Parent2:
+# #     def home(self):
+# #         print("from parent2")
+# # class Child(Parent2,Parent1): # mro
+# #     pass
+
+# # obj=Child()
+# # obj.home()
+
+
+
+# class Parent:
 #     def home(self):
 #         print("From parent")
-# class Parent2:
+# class Child(Parent):
 #     def home(self):
-#         print("from parent2")
-# class Child(Parent1,Parent2):
-#     pass
-
+#         print("from child")
+#         super().home()
 # obj=Child()
 # obj.home()
 
 
-
-# class Parent1:
-#     def home(self):
-#         print("From parent")
-# class Parent2:
-#     def home(self):
-#         print("from parent2")
-# class Child(Parent2,Parent1): # mro
-#     pass
-
-# obj=Child()
-# obj.home()
+# def rev(s):
+#     s1=""
+#     for i in range(len(s)-1,-1,-1):
+#         s1+=s[i]
+#     return s1
+# s=input("Enter a string :")
+# result=rev(s)
+# print(result)
 
 
+# def count_list(li):
+#     count=0
+#     for i in li:
+#         count+=1
+#     return count
+# li=eval(input("Enter any list :"))
+# result=count_list(li)
+# print(result)
 
-class Parent:
-    def home(self):
-        print("From parent")
-class Child(Parent):
-    def home(self):
-        print("from child")
-        super().home()
-obj=Child()
-obj.home()
 
 
 def rev(s):
@@ -44,14 +65,8 @@ def rev(s):
     return s1
 s=input("Enter a string :")
 result=rev(s)
-print(result)
+if s==result:
+    print("Palindrom")
+else:
+    print("Not a palindrom")
 
-
-def count_list(li):
-    count=0
-    for i in li:
-        count+=1
-    return count
-li=eval(input("Enter any list :"))
-result=count_list(li)
-print(result)
