5510ce8992dcd4aec48d15a592780722c6d0316e
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-11 13:28:44 +0530

    code of python

--- encapsulation.py
+++ encapsulation.py
@@ -11,15 +11,31 @@
 # print(Parent.principal)
 
 
+# class Parent:
+#     _principal='python'
+#     def _new(self):
+#         print('hello')
+# class Child(Parent):
+#     def child(self):
+#         print(Parent._principal)
+#         Parent._new(self)
+# obj=Child()
+# obj.child()
+# print(Parent._principal)
+
+
 class Parent:
-    _principal='python'
-    def _new(self):
+    __principal='python'
+    def __new(self):
         print('hello')
 class Child(Parent):
     def child(self):
-        print(Parent._principal)
-        Parent._new(self)
+        print(Parent.__principal)
+        Parent.__new(self)
 obj=Child()
-obj.child()
-print(Parent._principal)
+# obj.child()
+# print(Parent.__principal)
+# print(Parent.__new())
+# print(dir(Parent))
+print(Parent._Parent__principal)
 
