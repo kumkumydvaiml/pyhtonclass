b0733c6944558d82bea486f516ff69a393ca47af
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-18 00:39:57 +0530

    code

--- practi.py
+++ practi.py
@@ -1196,10 +1196,16 @@
 #     print()
 
 
+# n=5
+# num=1
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
 n=5
-num=1
 for i in range(1,n+1):
     for j in range(1,i+1):
-        print(num,end=" ")
-        num+=1
+        print("*",end=" ")
     print()
