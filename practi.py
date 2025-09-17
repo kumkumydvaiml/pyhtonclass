12561b017c1f920d6bb221074aff7ae93e8db706
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-18 00:33:57 +0530

    code

--- practi.py
+++ practi.py
@@ -1185,12 +1185,13 @@
 #     print("Not")
 
 
-n=5
-ch='A'
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(ch,end=" ")
-        ch=chr(ord(ch)+1)
-    if ch>'Z':
-        ch='A'
-    print()
+# n=5
+# ch='A'
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(ch,end=" ")
+#         ch=chr(ord(ch)+1)
+#     if ch>'Z':
+#         ch='A'
+#     print()
+
