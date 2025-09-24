b7520f24063fb541161e68e0226b45a9845af855
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-24 17:07:25 +0530

    code

--- armstrong.py
+++ armstrong.py
@@ -1,15 +1,30 @@
-n=(input("Enter number :"))
-n1=len(n)
-n=int(n)
-sum=0
-ans=n
-while n>0:
- digit=n%10
- sum=sum+digit**n1
- n=n//10
-if(sum==ans):
- print("Armstrom")
-else:
- print("not armstrom")
+# n=(input("Enter number :"))
+# n1=len(n)
+# n=int(n)
+# sum=0
+# ans=n
+# while n>0:
+#  digit=n%10
+#  sum=sum+digit**n1
+#  n=n//10
+# if(sum==ans):
+#  print("Armstrom")
+# else:
+#  print("not armstrom")
+
+
+#  s1,s2="python","language"
+#  s=','.join([s1,s2])
+#  print(s)
+
+
+# li=eval(input("Enter 5 name :"))
+# s="_".join("li")
+# print(s)
+
+s="python"
+x=s.count('')
+print(x)
+
 
 
