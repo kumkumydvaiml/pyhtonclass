ca674862ecac0292b08400f3aca2e0d735eeff03
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-25 17:34:27 +0530

    code

--- armstrong.py
+++ armstrong.py
@@ -92,4 +92,9 @@ n=int(input("Enter a number :"))
 for i in range(1,n+1):
     print(" "*(n-i)+"* "*i)
 
+n=int(input("Enter a number :"))
+for i in range(1,n+1):
+    print(" "*(2*n-i)+"* "*i)
+
+
 
