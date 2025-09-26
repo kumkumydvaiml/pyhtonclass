117cacaf0c0de7a1022609cd10c89403d5a40522
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-09-26 17:01:32 +0530

    code

--- armstrong.py
+++ armstrong.py
@@ -109,11 +109,21 @@
 #     s=chr(ord(s)+1)
 
 
+# n=int(input("Enter a number :"))
+
+# for i in range(1,n+1):
+#     s="A"
+#     for j in range(1,n+1):
+#         print(s,end=" ")
+#         s=chr(ord(s)+1)
+#     print()
+
+
 n=int(input("Enter a number :"))
 
 for i in range(1,n+1):
     s="A"
-    for j in range(1,n+1):
+    for j in range(1,i+1):
         print(s,end=" ")
         s=chr(ord(s)+1)
     print()
