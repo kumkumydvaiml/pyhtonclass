fce4b6976a6a8135df9e488764482ee02930241a
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-11-06 12:17:04 +0530

    pp.py

--- pp.py
+++ pp.py
@@ -439,10 +439,14 @@
 #         max=i
 # print(max)
 
-n=int(input("Enter a number :"))
-fact=1
-for i in range(1,n+1):
-    fact*=i
-print(fact)
+# n=int(input("Enter a number :"))
+# fact=1
+# for i in range(1,n+1):
+#     fact*=i
+# print(fact)
+
+
+
+
 
 
